b9be728984b8f48de5a99ba61c5ba2ddd8cfb352
akyadav1319-cell <ak.yadav1319@gmail.com>
2026-02-06 21:21:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// update slider values live
+const evSlider = document.getElementById("ev");
+const renewSlider = document.getElementById("renew");
+
+evSlider.oninput = () => {
+  document.getElementById("evVal").innerText = evSlider.value;
+};
+
+renewSlider.oninput = () => {
+  document.getElementById("renewVal").innerText = renewSlider.value;
+};
+
+function runSimulation() {
+  const ev = Number(document.getElementById("ev").value);
+  const renew = Number(document.getElementById("renew").value);
+
+  fetch("http://127.0.0.1:8000/simulate", {
+
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ev: ev,
+      renew: renew
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("co2").innerText = data.co2;
+    document.getElementById("pollution").innerText = data.pollution;
+    document.getElementById("report").innerText = data.report;
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+}
+
